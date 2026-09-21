169f69b66e7e05ee2d48d3e3887e49d3a8ef6cbc
Gaurav Sondhiya <gauravsondhiya1503@gmail.com>
2026-09-21 07:51:41 +0530

    deploy fixed

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,11 @@ const app = express()
 dbconnect()
 
 app.use(cookieParser());
-app.listen(3000)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 app.use (express.json())
 
 app.use(
